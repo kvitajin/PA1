ddc189af442d4ddbad6949d3732506ad07c12eae
Jindřich Kvita <jindrich.kvita@gnj.cz>
2020-12-08 19:49:30 +0100

    nejedeee

--- GoLCpp/main.cpp
+++ GoLCpp/main.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <thread>
 
+using namespace std;
+
 
 void print(std::vector<std::vector<int>> &arr){
     for (int i = 0; i < arr.size(); ++i) {
@@ -19,57 +21,62 @@ void print(std::vector<std::vector<int>> &arr){
     }
 
 }
-void lineInThread(std::vector<std::vector<int>> &arr; int line){
-	int counter = 0;
-	std::vector<int> tmpLine;
-	for(int cell = 0; cell< arr[line].size(); ++cell){
-	    surroundings(line, cell, arr, counter);
-	    if (arr[line][cell] && counter>2 ){                                   //otocenae znamenko >
-                tmpLine[cell]= 0;
-            }
-            else if (arr[line][cell] && (counter == 2 || counter == 3 )){                                                             tmpLine[cell]= 1;
-            }
-            else if (arr[line][cell] && counter >3){
-                tmpLine[cell]= 0;                                       }                                                       else if (!arr[line][cell] && counter==3 ){                        tmpLine[cell]= 1;                                       }
-	    
-        }
-}
+
 void surroundings(int &i, int &j, std::vector<std::vector<int>> &arr, int &counter) {
-	for (int k = i - 1; k < i + 2; ++k) {                       for (int l = j - 1; l < j + 2; ++l) {
+	for (int k = i - 1; k < i + 2; ++k) {
+	    for (int l = j - 1; l < j + 2; ++l) {
                     if (k >= 0 &&
                         l >= 0 &&
                         k < arr.size() &&                                       l < arr[0].size() &&
-                        (!(k == i && l == j))) {
-            //meze
-                        if (arr[k][l] == 1) {
-   //sousedi                                                                      
-   			++counter;
+                        (!(k == i && l == j))) { //meze
+                            if (arr[k][l] == 1) {    //sousedi
+                                ++counter;
+                            }
                         }
-                    }
-//
-                }
-            }
+	    }
+	}
 
 }
+void lineInThread(std::vector<std::vector<int>> &arr, int &line){
+    int counter = 0;
+    std::vector<int> tmpLine;
+    for(int cell = 0; cell< arr[line].size(); ++cell){
+        surroundings(line, cell, arr, counter);
+        if (arr[line][cell] && counter>2 ){                                   //otocenae znamenko >
+            tmpLine[cell]= 0;
+        }
+        else if (arr[line][cell] && (counter == 2 || counter == 3 )){
+            tmpLine[cell]= 1;
+        }
+        else if (arr[line][cell] && counter >3){
+        tmpLine[cell]= 0;
+        }
+        else if (!arr[line][cell] && counter==3 ){
+            tmpLine[cell]= 1;
+        }
+
+    }
+}
+
+
 void paralelRound(std::vector<std::vector<int>> &arr){
     std::vector<std::vector<int>> tmp;
-    tmp.resize(arr.size());
     std::vector<std::thread> threads;
     std::vector<int> row;
+
+    tmp.resize(arr.size());
     for (int i = 0; i < arr.size(); ++i) {
         tmp[i].resize(arr[i].size());
     }
-    for (int thread = 0; thread < arr.size(); ++thread){
 
-
+    for (int th = 0; th < arr.size(); ++th){
+        threads.push_back(std::thread(lineInThread,arr,th));
     }
 
-    for (int i = 0; i < arr.size(); ++i) {
-        for (int j = 0; j < arr[i].size(); ++j) {
-            
-        }
+    for (std::thread & th : threads){
+        if (th.joinable())
+            th.join();
     }
-
 }
 
 void round(std::vector<std::vector<int>> &arr){
@@ -116,7 +123,7 @@ int main() {
 
     print(playingBoard);
    // system("clear");
-    round(playingBoard);
+    paralelRound(playingBoard);
     print(playingBoard);
 
 
