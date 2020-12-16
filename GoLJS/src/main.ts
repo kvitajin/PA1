e49bc20df291523d6896aec3313fd01b55ad5836
Jindřich Kvita <jindrich.kvita@gnj.cz>
2020-12-16 13:15:40 +0100

    hotovo

--- GoLJS/src/main.ts
+++ GoLJS/src/main.ts
@@ -4,69 +4,63 @@ import { Worker } from 'worker_threads';
 import { resolve } from 'path';
 import { constants } from 'os';
 
-
-// let data: number[][]=[];
-// let matrixSize=2;
-// const workers: Worker[] = [];
-
-
-interface lineOfData{
-    id :number;
-    data:number[][];
+interface lineOfData {
+	id: number;
+	data: number[][];
 }
-async function paralelRound(args:lineOfData):Promise<number[]> {
-    return new Promise((resolve, reject) => {
-        const worker = new Worker(path.resolve(__dirname, './worker.js'), {
-            workerData: args,
-        });
-        worker.on('message', (msg) => {
-            resolve(msg);
-        });
+async function paralelRound(args: lineOfData): Promise<number[]> {
+	return new Promise((resolve, reject) => {
+		const worker = new Worker(path.resolve(__dirname, './worker.js'), {
+			workerData: args,
+		});
+		worker.on('message', (msg) => {
+			resolve(msg);
+		});
 
-        worker.on('error', (err) => {
-            reject(err);
-        });
-    });
+		worker.on('error', (err) => {
+			reject(err);
+		});
+	});
 }
-function print(data: number[][]){
-    for (let i=0; i<data.length; ++i) {
-        for (let j = 0; j < data[i].length; ++j) {
-            if (data[i][j]===1){
-                process.stdout.write(" ■ ");    //■
-            }
-            else{
-                process.stdout.write("   ");
-            }
-        }
-        console.log("|");
-    }
+function print(data: number[][]) {
+	for (let i = 0; i < data.length; ++i) {
+		for (let j = 0; j < data[i].length; ++j) {
+			if (data[i][j] === 1) {
+				process.stdout.write(' ■ '); //■
+			} else {
+				process.stdout.write('   ');
+			}
+		}
+		console.log('|');
+	}
 }
 function delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function main(){
-    let playingBoard: number[][]=  [[0, 0, 0, 0, 0, 0],
-                                    [0, 0, 0, 1, 0, 0],
-                                    [0, 1, 0, 0, 1, 0],
-                                    [0, 1, 0, 0, 1, 0],
-                                    [0, 0, 1, 0, 0, 0],
-                                    [0, 0, 0, 0, 0, 0]];
-    print(playingBoard);
-    while (true){
-        const jobs=[];
-        for (let i = 0; i < playingBoard.length; i++) {
-            jobs.push(paralelRound({id:i,data:playingBoard}))
-        }
-        const result = await Promise.all(jobs);
-        for (let i= 0; i< result.length; ++i){
-            playingBoard[i]=result[i];
-        }
-        console.clear();
-        print(playingBoard);
-        delay(1000);
-    }
-
+async function main() {
+	let playingBoard: number[][] = [
+		[0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 1, 0, 0],
+		[0, 1, 0, 0, 1, 0],
+		[0, 1, 0, 0, 1, 0],
+		[0, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0],
+	];
+	print(playingBoard);
+	while (true) {
+		const jobs = [];
+		for (let i = 0; i < playingBoard.length; i++) {
+			jobs.push(paralelRound({ id: i, data: playingBoard }));
+		}
+		const result = await Promise.all(jobs);
+		for (let i = 0; i < result.length; ++i) {
+			playingBoard[i] = result[i];
+		}
+		console.clear();
+		print(playingBoard);
+		delay(1000);
+	}
 }
 
 main();
--- GoLJS/src/worker.ts
+++ GoLJS/src/worker.ts
@@ -1,64 +1,33 @@
 import { parentPort, workerData } from 'worker_threads';
-let tmp: number[]=[];
+let tmp: number[] = [];
 
-// function surroundings(data: number[][], i:number, j:number){
-// 	let counter=0;
-// 	for (let k=i-1;k<i+2; ++k){
-// 		for (let l=j-1; l<j+2; ++l){
-// 			if (k >= 0 &&
-// 				l >= 0 &&
-// 				k < data.length &&
-// 				l < data[0].length &&
-// 				(!(k == i && l == j))){
-// 				if(data[k][l]==1){
-// 					++counter;
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return counter;
-//
-// }
-
-
-// console.log(workerData.data[workerData.id].length);
-
-for (let j=0; j<workerData.data[0].length; ++j){
-	let counter=0;
-	for (let k=workerData.id-1;k<workerData.id+2; ++k){
-		for (let l=j-1; l<j+2; ++l){
-			if (k >= 0 &&
+for (let j = 0; j < workerData.data[0].length; ++j) {
+	let counter = 0;
+	for (let k = workerData.id - 1; k < workerData.id + 2; ++k) {
+		for (let l = j - 1; l < j + 2; ++l) {
+			if (
+				k >= 0 &&
 				l >= 0 &&
 				k < workerData.data.length &&
 				l < workerData.data[0].length &&
-				(!(k == workerData.id && l == j))){
-				if(workerData.data[k][l]==1){
+				!(k == workerData.id && l == j)
+			) {
+				if (workerData.data[k][l] == 1) {
 					++counter;
 				}
 			}
 		}
 	}
-	// console.log(counter);
-	if (workerData.data[workerData.id][j] && counter<2 ){
+	if (workerData.data[workerData.id][j] && counter < 2) {
+		tmp.push(0);
+	} else if (workerData.data[workerData.id][j] && (counter == 2 || counter == 3)) {
+		tmp.push(1);
+	} else if (workerData.data[workerData.id][j] && counter > 3) {
+		tmp.push(0);
+	} else if (!workerData.data[workerData.id][j] && counter == 3) {
+		tmp.push(1);
+	} else {
 		tmp.push(0);
 	}
-	else if (workerData.data[workerData.id][j] && (counter == 2 || counter == 3 )){
-		tmp.push(1);
-	}
-	else if (workerData.data[workerData.id][j] && counter >3){
-		tmp.push(0);
-	}
-	else if (!workerData.data[workerData.id][j] && counter==3 ){
-		tmp.push(1);
-	}
-	else{
-		tmp.push(0)
-	}
-
 }
-
-
-// console.log([workerData.id, tmp]);
 parentPort?.postMessage(tmp);
-
-// parentPort?.postMessage(`Hi! I'm worker number ${workerData.id}, ${workerData.data}`);
